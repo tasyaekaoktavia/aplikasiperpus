cfdd87ffa0cf4befa1ddcd69e34b018ab03e2ce8
Tasya Oktavia <91074896+tasyaekaoktavia@users.noreply.github.com>
2022-05-16 13:49:01 +0700

    fix

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import PengunjungRoute from "./routes/PengunjungRoute.js";
 import DaftarbukuRoute from "./routes/DaftarbukuRoute.js";
 import PinjambukuRoute from "./routes/PinjambukuRoute.js";
 import path from "path";
-import logger from "morgan";
+import { fileURLToPath } from 'url';
 
 const app = express();
 mongoose.connect('mongodb+srv://tasya:tasya@perpustakaan.ull9f.mongodb.net/perpustakaan?retryWrites=true&w=majority',{
@@ -17,7 +17,6 @@ const db = mongoose.connection;
 db.on('error', (erorr) => console.log(erorr));
 db.once('open', () => console.log('Database Connected.....'));
 
-app.use(logger("dev"));
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended : false}));
@@ -25,11 +24,12 @@ app.use(PengunjungRoute);
 app.use(DaftarbukuRoute);
 app.use(PinjambukuRoute);
 
-app.use(express.static(path.join(__dirname, "./frontend/build")));
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+app.use(express.static(path.join(__dirname, './frontend/build')));
 
 app.get("*", function (_, res) {
     res.sendFile(
-        path.join(__dirname,"./frontend/build/index.html"),
+        path.join(__dirname,'./frontend/build/index.html'),
         function (erorr) {
             if (erorr) {
                 res.status(500).send(erorr);
