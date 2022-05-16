2ec87e9d5e69aefc4efce79630249af0e7564867
Tasya Eka OKtavia <91074896+tasyaekaoktavia@users.noreply.github.com>
2022-05-16 13:12:13 +0700

    uppdatess

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ import PengunjungRoute from "./routes/PengunjungRoute.js";
 import DaftarbukuRoute from "./routes/DaftarbukuRoute.js";
 import PinjambukuRoute from "./routes/PinjambukuRoute.js";
 import path from "path";
-import logger from "morgan";
 
 const app = express();
 mongoose.connect('mongodb+srv://tasya:tasya@perpustakaan.ull9f.mongodb.net/perpustakaan?retryWrites=true&w=majority',{
@@ -17,25 +16,20 @@ const db = mongoose.connection;
 db.on('error', (erorr) => console.log(erorr));
 db.once('open', () => console.log('Database Connected.....'));
 
-app.use(logger("dev"));
 app.use(cors());
 app.use(express.json());
 app.use(PengunjungRoute);
 app.use(DaftarbukuRoute);
 app.use(PinjambukuRoute);
 
-app.use(express.static(path.join(__dirname, "./frontend/build")));
-
-app.get("*", function (_, res) {
-    res.sendFile(
-        path.join(__dirname,"./frontend/build/index.html"),
-        function (erorr) {
-            if (erorr) {
-                res.status(500).send(erorr);
-            }
-        }
-    );
-});
+if (process.env.NODE_ENV=='production'){
+    const path = require('path')
+    
+    app.get('/',(req,res)=>{
+    app.use(express.static(path.resolve(__dirname,'frontend',build')))
+    res.sendFile(path.resolve(__dirname,'frontend',build','index.html'))
+    })
+}
 
 const port = process.env.PORT || 5000;
 app.listen ( port, ()=> console.log('server up and running...'));
